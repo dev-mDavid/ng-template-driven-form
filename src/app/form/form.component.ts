917601761ee918f2c937a86fc72d4a16b7742c01
mDavid <dev.mdavid@gmail.com>
2019-12-20 12:14:02 -0800

    convert & config all scss files to sass

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.scss']
+  styleUrls: ['./form.component.sass']
 })
 export class FormComponent implements OnInit {
 
